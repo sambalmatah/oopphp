ca06c6365607f53076a1fe673c7bbce8e8e1c21f
sambalmatah <yudiarinugroho14@gmail.com>
2025-11-09 00:01:52 +0700

    pertemuan 11 constant

---
+++ 11.constant.php
@@ -0,0 +1,46 @@
+<?php 
+
+// // konstanta yang tidak dapat digunakan di dalam class
+// define('NAMA', 'Yudi Ari');
+
+// echo NAMA;
+// echo "<br>";
+
+
+// // konstanta yang dapat digunakan di dalam class
+// const UMUR = 29;
+
+// echo UMUR;
+
+
+
+// // menerapkan konstanta dengan menggunakan class
+// class Coba {
+//     const NAMA = 'Yudi Ari';
+
+// }
+
+// echo Coba::NAMA;
+
+
+// Magic Constant
+// echo __LINE__; // mencari baris
+
+// echo __FILE__; // mencari letak file (path file)
+
+// function coba() {
+//     return __FUNCTION__;
+// }
+
+// echo coba(); // mencari nama function
+
+
+// class Coba {
+//     public $kelas = __CLASS__;
+// }
+
+// $obj = new Coba;
+// echo $obj->kelas; // mencari nama class
+
+
+?>
